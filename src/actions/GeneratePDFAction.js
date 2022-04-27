1008e83d5aa093390f92e7b93bca99f94c5e3bec
Mikael Åsbjørnsson-Stensland <mikael@grensesnitt.no>
2022-04-27 21:42:56 +0200

    Improvements

--- src/actions/GeneratePDFAction.js
+++ src/actions/GeneratePDFAction.js
@@ -2,34 +2,32 @@ import { jsPDF } from "jspdf";
 import { DocumentIcon } from "@sanity/icons";
 
 const flattenJSON = (obj = {}, res = {}, extraKey = "") => {
-    for (key in obj) {
-      if (typeof obj[key] !== "object") res[extraKey + key] = obj[key];
-      else flattenJSON(obj[key], res, `${extraKey}${key}.`);
-    }
+    Object.entries(obj).forEach((entry) => {
+      if (typeof entry[1] !== "object") res[extraKey + entry[0]] = entry[1];
+      else flattenJSON(entry[1], res, `${extraKey}${entry[0]}.`);
+    });
 
     return res;
   },
   parser = (data, text) => {
+    if (data.title) text += "\n" + data.title.toUpperCase() + "\n\n\n";
     Object.entries(flattenJSON(data)).forEach((entry) => {
       if (
+        entry[0] !== "title" &&
         entry[0].indexOf("_") !== 0 &&
         entry[0].indexOf("._") === -1 &&
         typeof entry[1] === "string" &&
-        entry[1].indexOf(" ") !== -1 &&
-        entry[1].length > 1
+        entry[1].length > 0
       )
-        text +=
-          "%gray%" +
-          entry[0].replace(/\./g, " > ") +
-          ":\n\n" +
-          entry[1] +
-          "\n\n";
+        text += entry[1] + "\n\n";
     });
 
     return text;
   };
 
 export const GeneratePDFAction = (props) => {
+  if (props.published === null) return;
+
   const { published } = props,
     pageWidth = 8.3,
     lineHeight = 1.15,
@@ -39,27 +37,24 @@ export const GeneratePDFAction = (props) => {
     ptsPerInch = 72,
     oneLineHeight = (fontSize * lineHeight) / ptsPerInch,
     doc = new jsPDF({ unit: "in", lineHeight }),
-    text = parser(_data, ""),
-    textLines = doc
-      .setFont("helvetica")
-      .setFontSize(fontSize)
-      .splitTextToSize(text, maxLineWidth),
+    text = parser(published, ""),
     maxLines = parseInt((11.7 - margin * 2) / oneLineHeight);
+  let textLines = doc
+    .setFont("helvetica")
+    .setFontSize(fontSize)
+    .splitTextToSize(text.replace(/\u2028/g, ""), maxLineWidth);
 
+  if (textLines.length === 0)
+    textLines = [
+      "It looks like you haven't got any text fields in your schema. PDF Generator will render the content of any fields of type 'text' in alphabetical order based on field name. If a field named 'title' is present, it will render its value first, styled as a heading.",
+    ];
+  textLines = textLines.filter((line) => line.slice(0, 1) !== "@");
   for (let i = 0; i < textLines.length / maxLines; i++) {
     if (i > 0) doc.addPage("a4");
     for (let j = 0; j < maxLines; j++) {
       if (textLines[i * maxLines + j]) {
-        if (textLines[i * maxLines + j].indexOf("%gray%") === 0)
-          doc.setTextColor(150);
-        else doc.setTextColor(0);
         doc.text(
-          textLines[i * maxLines + j].indexOf("%gray%") === 0
-            ? textLines[i * maxLines + j]
-                .replace("%gray%", "")
-                .toUpperCase()
-                .replace(/\_/g, " ")
-            : textLines[i * maxLines + j],
+          textLines[i * maxLines + j],
           margin,
           margin + j * oneLineHeight
         );
@@ -70,6 +65,6 @@ export const GeneratePDFAction = (props) => {
   return {
     label: "Generate PDF",
     icon: DocumentIcon,
-    onHandle: () => doc.save(`${published.title || published._id}.pdf`),
+    onHandle: () => doc.save(`${published._id}.pdf`),
   };
 };
